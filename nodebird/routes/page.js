84d0a2bf51015e4a4d7f956e65cac1ba8365773f
jszeronine <kineage18@naver.com>
2019-08-19 21:48:15 +0900

    SNS study

---
+++ nodebird/routes/page.js
@@ -0,0 +1,29 @@
+const express = require( 'express' );
+const router = express.Router();
+
+
+// 프로필 페이지
+router.get( '/profile', ( req, res ) => {
+    res.render( 'profile', { title : '내 정보 - NodeBird', user : null });
+});
+
+// 회원가입 페이지
+router.get( '/join', ( req, res ) => {
+    res.render( 'join', {
+        title : '회원가입 - NodeBird',
+        user : null,
+        joinError : req.flash( 'joinError' )
+    });
+})
+
+// 메인페이지
+router.get( '/', ( req, res, next ) => {
+    res.render( 'main', {
+        title : 'NodeBird',
+        twits : [],
+        user : null,
+        loginError : req.flash( 'loginError' )
+    });
+});
+
+module.exports = router;
