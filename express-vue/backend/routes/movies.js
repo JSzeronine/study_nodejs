dbc1a28f2c101aaf5bd27ad3f7bf7e011a02be59
jszeronine <kineage18@naver.com>
2019-09-05 21:55:44 +0900

    vue-express setting

---
+++ express-vue/backend/routes/movies.js
@@ -0,0 +1,19 @@
+const express = require( 'express' );
+const router = express.Router();
+const movies = require( '../movies.json' );
+
+router.get( '/', ( req, res, next ) => {
+    res.send( movies );
+});
+
+router.get( '/:id', ( req, res, next ) => {
+    var id = parseInt( req.params.id, 10 );
+    var movie = movies.filter(( movie ) => {
+        return movie.id === id;
+    });
+
+    res.send( movie );
+});
+
+module.exports = router;
+
