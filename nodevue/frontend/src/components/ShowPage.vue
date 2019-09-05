4c0dc1ac338ac46ca9758eea97eecb6e029ab40e
LOTTEWORLD\dev2057 <kineage18@naver.com>
2019-09-05 09:22:35 +0900

    node vue 연결

---
+++ nodevue/frontend/src/components/ShowPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>상세 내용</h1>
+    <div>
+        <img :src="movie.poster" alt="">
+        <p>{{ movie.id }}</p>
+        <p>{{ movie.name }}</p>
+        <p>{{ movie.year }}</p>
+        <p>{{ movie.director }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created: function () {
+    var id = this.$route.params.id
+    this.$http.get(`/api/movies/${id}`)
+    .then((response) => {
+      this.movie = response.data[ 0 ];
+
+      console.log( this.movie );
+    })
+  },
+  data: function () {
+    return {
+      movie: {}
+    }
+  }
+}
+</script>
