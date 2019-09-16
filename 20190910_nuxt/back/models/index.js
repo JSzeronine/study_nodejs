e8b225bd54328f2adcef2535d033d9dbf2f74abf
jszeronine <kineage18@naver.com>
2019-09-16 22:21:45 +0900

    nodejs setting
    sequelize
    nodemon

---
+++ 20190910_nuxt/back/models/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+let sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
--- 20190910_nuxt/info.js
+++ 20190910_nuxt/info.js
@@ -1,7 +1,7 @@
 
 
 
-
+// -------- frontend ---------
 // npm i vue nuxt
 // npm i @nuxtjs/vuetify vuetify 
 // npm i @nuxtjs/axios axios
@@ -11,4 +11,31 @@
 // fetch
 // watch
 
+// npm outdated
+// npm update
+// npm audit fix // 취약점 고치기
 
+
+
+
+
+// ------- backend -----
+// npm init
+// npm i express
+// npm i sequelize
+// npm i mysql2
+// npm i -D sequelize-cli
+// npx sequelize init
+
+// npm i -D nodemon
+// -> package.json : "dev" : "nodemon app.js"
+
+
+
+
+// GET : 가져오다
+// POST : 생성하다
+// PUT : 전체 수정
+// PATCH : 부분 수정
+// DELETE : 삭제
+// OPTIONS : 찔러보기
